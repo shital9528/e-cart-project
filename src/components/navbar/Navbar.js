560b629e8ad8c223c05eb6de46a1932070ae573d
Shital Kadam <shitalkadam9528@gmail.com>
2023-01-05 00:22:19 +0530

    project

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
+
+
+    </div>
+  );
+};
+
+export default Navbar;
